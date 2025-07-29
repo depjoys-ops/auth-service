78f5a7a4420b48af075af42dbb384f8917973663
Oleh Kuzmych <depjoys@gmail.com>
2025-07-29 12:07:24 +0000

    update logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -2,9 +2,8 @@ package logger
 
 import (
 	"io"
-	"log/slog"
+	"log"
 	"os"
-	"strings"
 )
 
 type Logger interface {
@@ -12,58 +11,7 @@ type Logger interface {
 	Info(msg string, args ...any)
 	Warn(msg string, args ...any)
 	Error(msg string, args ...any)
-}
-
-type logger struct {
-	log *slog.Logger
-}
-
-func NewSlogLogger(filePath, level string) (Logger, error) {
-
-	out, err := output(filePath)
-	if err != nil {
-		return nil, err
-	}
-
-	handler := slog.NewJSONHandler(out, &slog.HandlerOptions{
-		AddSource: true,
-		Level:     slogLevel(level),
-	})
-
-	return &logger{
-		log: slog.New(handler),
-	}, nil
-}
-
-func (l *logger) Debug(msg string, args ...any) {
-	l.log.Debug(msg, args...)
-}
-
-func (l *logger) Info(msg string, args ...any) {
-	l.log.Info(msg, args...)
-}
-
-func (l *logger) Warn(msg string, args ...any) {
-	l.log.Warn(msg, args...)
-}
-
-func (l *logger) Error(msg string, args ...any) {
-	l.log.Error(msg, args...)
-}
-
-func slogLevel(level string) slog.Level {
-	switch strings.ToLower(level) {
-	case "debug":
-		return slog.LevelDebug
-	case "info":
-		return slog.LevelInfo
-	case "warn":
-		return slog.LevelWarn
-	case "error":
-		return slog.LevelError
-	default:
-		return slog.LevelInfo
-	}
+	ServerErrorLog() *log.Logger
 }
 
 func output(filePath string) (io.Writer, error) {
---
+++ pkg/logger/slog.go
@@ -0,0 +1,64 @@
+package logger
+
+import (
+	"log"
+	"log/slog"
+	"strings"
+)
+
+type logger struct {
+	log *slog.Logger
+}
+
+var _ Logger = (*logger)(nil)
+
+func NewSlogLogger(filePath, level string) (*logger, error) {
+	out, err := output(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	handler := slog.NewJSONHandler(out, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slogLevel(level),
+	})
+
+	return &logger{
+		log: slog.New(handler),
+	}, nil
+}
+
+func (l *logger) Debug(msg string, args ...any) {
+	l.log.Debug(msg, args...)
+}
+
+func (l *logger) Info(msg string, args ...any) {
+	l.log.Info(msg, args...)
+}
+
+func (l *logger) Warn(msg string, args ...any) {
+	l.log.Warn(msg, args...)
+}
+
+func (l *logger) Error(msg string, args ...any) {
+	l.log.Error(msg, args...)
+}
+
+func (l *logger) ServerErrorLog() *log.Logger {
+	return slog.NewLogLogger(l.log.Handler(), slog.LevelError)
+}
+
+func slogLevel(level string) slog.Level {
+	switch strings.ToLower(level) {
+	case "debug":
+		return slog.LevelDebug
+	case "info":
+		return slog.LevelInfo
+	case "warn":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
+}
